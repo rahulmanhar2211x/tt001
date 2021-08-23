94a0159c3bed6a17d67276b68470d78bbed4bf91
Rahul Manhar <79684834+rahulmanhar2211x@users.noreply.github.com>
2021-08-23 14:01:09 +0530

    Update HandleManager.py

--- tortoolkit/core/HandleManager.py
+++ tortoolkit/core/HandleManager.py
@@ -623,12 +623,12 @@ async def about_me(message):
     diff = human_format.human_readable_timedelta(diff)
 
     msg = (
-        "<b>Name</b>: <code>TorToolkit</code>\n"
+        "<b>Name</b>: <code>TelegramToolkit</code>\n"
         f"<b>Version</b>: <code>{__version__}</code>\n"
         f"<b>Telethon Version</b>: {telever}\n"
         f"<b>Pyrogram Version</b>: {pyrover}\n"
         "<b>Created By</b>: @yaknight\n\n"
-        "<b>Modified</b>: @XcodersHub\n\n"
+        "<b>Modified</b>: @mirrorxy\n\n"
         "<u>Currents Configs:-</u>\n\n"
         f"<b>Bot Uptime:-</b> {diff}\n"
         "<b>Torrent Download Engine:-</b> <code>qBittorrent [4.3.0 fix active]</code> \n"
